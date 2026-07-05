e27cac5124eee794fed7ce6278568fe433c96af5
Dhairya gehlot <gehlotdhairya2@gmail.com>
2026-07-05 12:02:19 +0530

    Add files via upload

---
+++ Python/Dg.py/dg.1/dg.2/task1.py
@@ -0,0 +1,71 @@
+#1
+# num = int(input("Enter number"))
+# if num % 2 == 0:
+#     print("Even")
+# else:
+#     print("Odd")
+
+#2
+# num = float (input("Enter the no."))
+# if num > 0:
+#     print("No. is positive")
+# elif num < 0:
+#     print("No. is negative")
+# else:
+#     print("nO. is 0")
+
+#3
+# age = int(input("Enter number"))
+# if age>18:
+#      print("Eligible to vote")
+# else:
+#     print("Not eligible to vote")
+
+#4
+# a = int(input("Enter first number: "))
+# b = int(input("Enter second number: "))
+# if a > b:
+#     print("A is greater")
+# else:
+#     print("B is greater")
+#5
+# num = int(input("Enter a number"))
+# if num % 5== 0:
+#     print("Divisible by 5")
+# else:
+#     print("Not divisible by 5")
+
+# num = int(input("Enter a number"))
+# if num % 3 == 0:
+#     print("Divisible by 3")
+# else:
+#     print("Not divisible by 3")
+   
+
+# num = int(input("Enter a number"))
+# if num % 5== 0  and  num % 3 == 0:
+#     print("Divisible by both 3 and 5")
+# else:
+#     print("Not divisible by 3 and 5 ")
+
+
+
+# age = int(input("Enter your age: "))
+
+
+# if 18 <= age <= 60:
+#     print("Working age")
+# else:
+#     print("Not working age")
+
+
+marks = int(input("Enter a number"))
+
+if marks >= 90:
+    print("Grade A")
+elif marks >= 75:
+    print("Grade B")
+elif marks >= 50 :
+    print("Grade C")
+else:
+    print("Fail")
